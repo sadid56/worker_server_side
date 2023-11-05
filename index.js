035cbd8c1fd583b40bc9196e5be10e98f8255b0e
sadid56 <sadidhasan56@gmail.com>
2023-11-05 10:37:17 +0600

    add a get job and post job

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
+app.use(express.json())
 
 //4daB0TThbYAlbhgR
 //assignment-11
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://assignment-11:4daB0TThbYAlbhgR@cluster0.dzbhwpo.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +29,43 @@ async function run() {
     await client.connect();
 
     const JobsCollection = client.db('assignment-11').collection('jobs')
+    const bidsCollection = client.db('assignment-11').collection('bids')
 
     //! CRUD Operation
+    // job related
+    // post
+    app.post('/jobs', async(req, res)=>{
+      const addJobs = req.body;
+      const result = await JobsCollection.insertOne(addJobs)
+      res.send(result)
+    })
+
+
     //get
     app.get('/jobs', async(req, res)=>{
         const result = await JobsCollection.find().toArray()
         res.send(result)
+    })
+
+    //get specifiec data
+    app.get('/jobs/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await JobsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // bid related
+    //post bids
+    app.post('/bids', async(req, res)=>{
+      const bids = req.body;
+      const result = await bidsCollection.insertOne(bids)
+      res.send(result)
+    })
+    //get bids
+    app.get('/bids', async(req, res)=>{
+      const result = await bidsCollection.find().toArray()
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
