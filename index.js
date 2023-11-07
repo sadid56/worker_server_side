161837e53a000904260af2f43121efbfda325ade
sadid56 <sadidhasan56@gmail.com>
2023-11-07 20:03:20 +0600

    update vercel deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://assignment-11-96d26.web.app'],
   credentials: true
 }));
 app.use(express.json());
